198d6a476884fc4e8d1164c69f207b59f8d165d6
NahidHasan131 <nahid7532140@gmail.com>
2026-01-12 17:27:02 +0600

    hero section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Hero from "./Components/Hero/Hero"
+import Services from "./Components/Services/Services"
 
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <main className="overflow-x-hidden bg-white text-dark">
       <Hero></Hero>
+      <Services></Services>
     </main>
   )
 }
